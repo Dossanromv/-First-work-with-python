1ff20fa3f6ba1a5ec7e25818c0d9b7595b213ed5
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2024-08-24 12:27:43 +0500

    we added a loop

--- script.py
+++ script.py
@@ -1,8 +1,18 @@
 
-user = input('Введите имя: ')
-if user == 'Миша':
-    print('УРААААААААА МИША')
-elif user == 'Марс':
-    print('МАРС МАРС МАРС')
+# user = input('Введите имя: ')
+# if user == 'Миша':
+#     print('УРААААААААА МИША')
+# elif user == 'Марс':
+#     print('МАРС МАРС МАРС')
+# else:
+#     print('Hello,', user)
+
+
+i = 0
+while i < 5:
+    if i == 3:
+        break
+    i = i + 1
 else:
-    print('Hello,', user)
+        print('ХАХАХАХАХА')
+print(i)
